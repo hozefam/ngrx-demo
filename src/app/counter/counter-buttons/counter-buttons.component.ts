9a6cbebd68b53c33ce8213d4fc07f692fc264e93
Hozefa Mangalorewala <hozefam@gmail.com>
2021-10-29 11:22:58 +0300

    Initial Counter Setup

---
+++ src/app/counter/counter-buttons/counter-buttons.component.ts
@@ -0,0 +1,33 @@
+import { Component, EventEmitter, OnInit, Output } from '@angular/core';
+
+@Component({
+  selector: 'app-counter-buttons',
+  templateUrl: './counter-buttons.component.html',
+  styleUrls: ['./counter-buttons.component.css'],
+})
+export class CounterButtonsComponent implements OnInit {
+  @Output()
+  increment = new EventEmitter<void>();
+
+  @Output()
+  decrement = new EventEmitter<void>();
+
+  @Output()
+  reset = new EventEmitter<void>();
+
+  constructor() {}
+
+  ngOnInit(): void {}
+
+  OnIncrement = () => {
+    this.increment.emit();
+  };
+
+  OnDecrement = () => {
+    this.decrement.emit();
+  };
+
+  OnReset = () => {
+    this.reset.emit();
+  };
+}
